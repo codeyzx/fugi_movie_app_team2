f4ee0af1e668307aa5588ff7d529857145ae6200
syahadae <rasyidsyahada@gmail.com>
2022-08-19 14:48:12 +0700

    Movie Detail Update from mas Bima

--- lib/src/features/movie_detail/presentation/movie_detail_screen.dart
+++ lib/src/features/movie_detail/presentation/movie_detail_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fugi_movie_app_team2/src/common_config/app_theme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MovieDetailScreen extends StatelessWidget {
-  const MovieDetailScreen({Key? key}) : super(key: key);
+  MovieDetailScreen({Key? key}) : super(key: key);
   static const routeName = 'movie-detail-screen';
+  double SliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -40,29 +42,72 @@ class MovieDetailScreen extends StatelessWidget {
                     Positioned(
                       top: 160,
                       right: 0,
-                      child: Container(
-                        padding: const EdgeInsets.all(10),
-                        margin: const EdgeInsets.all(15),
-                        width: 75,
-                        decoration: BoxDecoration(
-                          color: AppTheme.primaryColor,
-                          borderRadius: BorderRadius.circular(50),
-                          border: Border.all(color: Colors.orange),
-                        ),
-                        child: Center(
-                          child: Row(
-                            children: const [
-                              Icon(
-                                Icons.star_border,
-                                color: Colors.orange,
-                              ),
-                              Text(
-                                '9.5',
-                                style: TextStyle(
+                      child: GestureDetector(
+                        onTap: () {
+                          //Navigator.pop(context);
+                          showModalBottomSheet(
+                              context: context,
+                              backgroundColor: Colors.transparent,
+                              builder: ((BuildContext context) {
+                                return Wrap(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(20),
+                                              topRight: Radius.circular(20))),
+                                      padding: EdgeInsets.all(20),
+                                      child: WidgetSLider(),
+                                    )
+                                  ],
+                                );
+                              }));
+                        },
+                        // child: Container(
+                        //   padding: const EdgeInsets.all(10),
+                        //   margin: const EdgeInsets.all(15),
+                        //   width: 75,
+                        //   decoration: BoxDecoration(
+                        //     color: AppTheme.primaryColor,
+                        //     borderRadius: BorderRadius.circular(50),
+                        //     border: Border.all(color: Colors.orange),
+                        //   ),
+                        //   child: Center(
+                        //     child: Row(
+                        //       children: [
+                        //         const Icon(
+                        //           Icons.star_border,
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.all(15),
+                          width: 75,
+                          decoration: BoxDecoration(
+                            color: AppTheme.primaryColor,
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(color: Colors.orange),
+                          ),
+                          child: Center(
+                            child: Row(
+                              children: const [
+                                Icon(
+                                  Icons.star_border,
                                   color: Colors.orange,
                                 ),
-                              ),
-                            ],
+                                Text(
+                                  '9.5',
+                                  style: TextStyle(
+                                    color: Colors.orange,
+                                  ),
+                                ),
+                                Text(
+                                  '9.5',
+                                  style: TextStyle(
+                                    color: Colors.orange,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -81,7 +126,8 @@ class MovieDetailScreen extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           ClipRRect(
-                            borderRadius: const BorderRadius.all(Radius.circular(10)),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
                             child: Image.asset(
                               'assets/images/movie-sub-hero.png',
                               fit: BoxFit.cover,
@@ -105,17 +151,44 @@ class MovieDetailScreen extends StatelessWidget {
                   width: MediaQuery.of(context).size.width * 0.8,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
+
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      const Icon(Icons.calendar_month,
+                          color: AppTheme.fourthColor),
+                      const Text('2022',
+                          style: TextStyle(
+                              color: AppTheme.fourthColor, fontSize: 18)),
+                      const Divider(
+                        color: AppTheme.fourthColor,
+                        thickness: 1,
+                      ),
+                      const Icon(Icons.watch_later,
+                          color: AppTheme.fourthColor),
+                      const Text(' 180 Minuts',
+                          style: TextStyle(
+                              color: AppTheme.fourthColor, fontSize: 18)),
+                      const Icon(Icons.airplane_ticket,
+                          color: AppTheme.fourthColor),
+                      const Text('Action',
+                          style: TextStyle(
+                              color: AppTheme.fourthColor, fontSize: 18)),
                       Icon(Icons.calendar_month, color: AppTheme.fourthColor),
-                      Text('2022', style: TextStyle(color: AppTheme.fourthColor, fontSize: 18)),
+                      Text('2022',
+                          style: TextStyle(
+                              color: AppTheme.fourthColor, fontSize: 18)),
                       Divider(
                         color: AppTheme.fourthColor,
                         thickness: 1,
                       ),
                       Icon(Icons.watch_later, color: AppTheme.fourthColor),
-                      Text(' 180 Minuts', style: TextStyle(color: AppTheme.fourthColor, fontSize: 18)),
+                      Text(' 180 Minuts',
+                          style: TextStyle(
+                              color: AppTheme.fourthColor, fontSize: 18)),
                       Icon(Icons.airplane_ticket, color: AppTheme.fourthColor),
-                      Text('Action', style: TextStyle(color: AppTheme.fourthColor, fontSize: 18)),
+                      Text('Action',
+                          style: TextStyle(
+                              color: AppTheme.fourthColor, fontSize: 18)),
                     ],
                   ),
                 )
@@ -161,6 +234,83 @@ class MovieDetailScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class WidgetSLider extends StatefulWidget {
+  const WidgetSLider({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetSLider> createState() => _WidgetSLiderState();
+}
+
+class _WidgetSLiderState extends State<WidgetSLider> {
+  double SliderValue = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Rate this Movie",
+            style: GoogleFonts.montserrat().copyWith(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                color: AppTheme.thirdColor)),
+        SizedBox(height: 20),
+        Text('${double.parse(SliderValue.toStringAsFixed(1))}',
+            style: GoogleFonts.montserrat().copyWith(
+                fontSize: 32,
+                fontWeight: FontWeight.w400,
+                color: AppTheme.thirdColor)),
+        SizedBox(height: 10),
+        SliderTheme(
+          data: SliderThemeData(
+            activeTrackColor: Color(0XFFFF8700),
+            trackHeight: 10,
+            overlayColor: Colors.amber,
+            inactiveTrackColor: Colors.grey,
+            thumbColor: Colors.white,
+            thumbShape:
+                RoundSliderThumbShape(elevation: 10, enabledThumbRadius: 15),
+            overlayShape: RoundSliderOverlayShape(overlayRadius: 20),
+          ),
+          // thumbColor: Colors.green,
+          // thumbShape: RoundSliderThumbShape(enabledThumbRadius: 20)),
+          child: Slider(
+            value: SliderValue,
+            max: 10,
+            // divisions: 100,
+            label: '${SliderValue.round()}',
+            // thumbColor: Colors.white,
+            // activeColor: Colors.amber,
+            autofocus: true,
+            //inactiveColor: Colors.grey,
+            onChanged: (double value) {
+              setState(() {
+                SliderValue = value;
+              });
+            },
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(28),
+          child: Container(
+            width: 220,
+            height: 56,
+            color: Color(0XFF0296E5),
+            child: Center(
+              child: Text("OK",
+                  style: GoogleFonts.montserrat().copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white)),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
