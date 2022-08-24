59223302cf891273a6ce89acbbb9ff306bd059f0
syahadae <rasyidsyahada@gmail.com>
2022-08-24 14:38:56 +0700

    add popular

---
+++ lib/src/features/home/domain/popular.g.dart
@@ -0,0 +1,45 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'popular.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Popular _$$_PopularFromJson(Map<String, dynamic> json) => _$_Popular(
+      adult: json['adult'] as bool?,
+      backdropPath: json['backdrop_path'] as String?,
+      genreIds:
+          (json['genre_ids'] as List<dynamic>?)?.map((e) => e as int).toList(),
+      id: json['id'] as int?,
+      originalLanguage: json['orignal_language'] as String?,
+      originalTitle: json['original_title'] as String?,
+      overview: json['overview'] as String?,
+      popularity: (json['popularity'] as num?)?.toDouble(),
+      posterPath: json['poster_path'] as String?,
+      releaseDate: json['release_date'] == null
+          ? null
+          : DateTime.parse(json['release_date'] as String),
+      title: json['title'] as String?,
+      video: json['video'] as bool?,
+      voteAverage: (json['vote_avarage'] as num?)?.toDouble(),
+      voteCount: json['vote_count'] as int?,
+    );
+
+Map<String, dynamic> _$$_PopularToJson(_$_Popular instance) =>
+    <String, dynamic>{
+      'adult': instance.adult,
+      'backdrop_path': instance.backdropPath,
+      'genre_ids': instance.genreIds,
+      'id': instance.id,
+      'orignal_language': instance.originalLanguage,
+      'original_title': instance.originalTitle,
+      'overview': instance.overview,
+      'popularity': instance.popularity,
+      'poster_path': instance.posterPath,
+      'release_date': instance.releaseDate?.toIso8601String(),
+      'title': instance.title,
+      'video': instance.video,
+      'vote_avarage': instance.voteAverage,
+      'vote_count': instance.voteCount,
+    };
